51bca8b330d9a951702e8c5b97b90a3724d044b1
JorgeVargas03 <joluvargaspa@ittepic.edu.mx>
2025-04-15 17:32:34 -0700

    Cambio de nombre

--- backend/routes/paymentsRoutes.js
+++ backend/routes/paymentsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controllerDeb = require("../controllers/debController");
+const controllerDeb = require("../controllers/paymentsController");
 
 //Esta implementacion queda temporalmente desactivada hasta se llegue a la parte de autenticacion de usuarios
 //const { authenticateToken } = require('../middleware/authMiddleware');
