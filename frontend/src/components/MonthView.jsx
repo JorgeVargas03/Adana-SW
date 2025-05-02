fd3f8391d90a9be5c80c37da7b6fc3a67d17cb87
JorgeVargas03 <joluvargaspa@ittepic.edu.mx>
2025-05-01 22:28:12 -0700

    Mostrar correctamente los eventos guardados

--- frontend/src/components/MonthView.jsx
+++ frontend/src/components/MonthView.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { isTokenValid } from '../utils/auth';
 import { useNavigate, Link } from 'react-router-dom';
-import { format, startOfMonth, endOfMonth, getDay, isSameDay, eachDayOfInterval,startOfDay, parseISO, } from 'date-fns';
+import { format, startOfMonth, endOfMonth, getDay, isSameDay, eachDayOfInterval, startOfDay, parseISO, } from 'date-fns';
 import { motion, AnimatePresence } from 'framer-motion';
 import { es } from 'date-fns/locale';
 import { useCarrito } from '../context/CarritoContext';
@@ -41,16 +41,16 @@ function MonthView({ month }) {
         const fetchedEvents = response.data.map(clase => {
           const [hour, minute] = clase.time.split(':');
           const [year, month, day] = clase.date.split('-').map(Number);
-          
+
           // Construimos fecha/hora como objeto local
           const dateWithTime = new Date(year, month - 1, day, parseInt(hour), parseInt(minute));
-  
+
           console.log('Evento generado:', {
             fechaOriginal: clase.date,
             horaOriginal: clase.time,
             fechaFinal: dateWithTime.toString()
           });
-  
+
           return {
             classId: clase.id,
             title: clase.title,
@@ -62,7 +62,7 @@ function MonthView({ month }) {
             date: dateWithTime
           };
         });
-  
+
         setEvents(fetchedEvents);
       } catch (error) {
         console.error("Error al cargar eventos:", error);
@@ -70,7 +70,7 @@ function MonthView({ month }) {
     };
     fetchEvents();
   }, []);
-  
+
 
   return (
     <div className="bg-[#F5F0FF] font-outfit rounded-3xl shadow-lg p-6 w-full h-full relative">
@@ -80,7 +80,7 @@ function MonthView({ month }) {
 
       <div className="grid grid-cols-7 gap-2 mb-4">
         {weekDays.map((day, idx) => (
-          <div key={idx} className="text-center text-[#A18CD1] font-semibold text-sm"> 
+          <div key={idx} className="text-center text-[#A18CD1] font-semibold text-sm">
             {day}
           </div>
         ))}
@@ -88,7 +88,7 @@ function MonthView({ month }) {
 
       <div className="grid grid-cols-7 gap-1 ">
         {Array.from({ length: firstDayIndex }).map((_, idx) => (
-          <div key={`empty-${idx}`}  /> 
+          <div key={`empty-${idx}`} />
         ))}
 
         <AnimatePresence>
@@ -96,18 +96,17 @@ function MonthView({ month }) {
             const isToday = isSameDay(day, new Date());
             const dayEvents = events.filter(e =>
               isSameDay(startOfDay(e.date), startOfDay(day))
-            );         
-            
+            );
+
             if (format(day, 'yyyy-MM-dd') === '2025-04-30') {
               console.log('Eventos del 30 de abril:', dayEvents);
             }
-          
+
             return (
               <motion.div
                 key={day}
-                className={`bg-white border border-[#E9DFFB] p-3 rounded-2xl min-h-[150px] flex flex-col items-center cursor-pointer hover:bg-[#f2e9fc] ${
-                  isToday ? 'ring-2 ring-[#7E5EC3] bg-[#f6f0ff]' : ''
-                }`}
+                className={`bg-white border border-[#E9DFFB] p-3 rounded-2xl min-h-[150px] flex flex-col items-center cursor-pointer hover:bg-[#f2e9fc] ${isToday ? 'ring-2 ring-[#7E5EC3] bg-[#f6f0ff]' : ''
+                  }`}
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0 }}
@@ -123,7 +122,7 @@ function MonthView({ month }) {
                 <div className="flex flex-col gap-1 w-full">
                   {dayEvents.map((event, idx) => (
                     <div
-                      key={idx} 
+                      key={idx}
                       className={`text-xs text-ellipsis px-2 py-1 rounded-md text-center font-semibold ${getEventColor(event.availableSpots)}`}
                       onClick={(e) => {
                         e.stopPropagation();
@@ -131,7 +130,7 @@ function MonthView({ month }) {
                         setShowModal(true);
                       }}
                     >
-                      
+
                       {event.title}
                     </div>
                   ))}
@@ -171,8 +170,8 @@ function MonthView({ month }) {
                 className="bg-white p-8 rounded-3xl shadow-2xl text-center max-w-sm mx-auto"
                 onClick={(e) => e.stopPropagation()}
               >
-                
-                {hasToken ? ( 
+
+                {hasToken ? (
                   selectedEvent && (
                     <>
                       <h2 className="text-2xl font-bold text-[#C3C37E] mb-4">{selectedEvent.title}</h2>
@@ -182,31 +181,30 @@ function MonthView({ month }) {
                       <p className="text-sm text-gray-700 mb-4"><strong>Reservados:</strong> {selectedEvent.capacity - selectedEvent.availableSpots} / {selectedEvent.capacity}</p> {/*capacity-availablespots pa ver cuantos son*/}
                       {/*Boton para unirse*/}
                       <button
-                            className="bg-[#C3C37E] hover:bg-[#5e46a5] text-white px-6 py-2 rounded-full font-semibold transition cursor-pointer"
-                            onClick={() => {
-                              agregarEvento({
-                                classId: selectedEvent.classId,
-                                id: selectedEvent.id, 
-                                instructorId: selectedEvent.instructorId,
-                                title: selectedEvent.title,
-                                description: selectedEvent.description,
-                                instructorName: selectedEvent.instructor,
-                                formattedDate: format(selectedEvent.date, "dd/MM/yyyy HH:mm"),
-                              });
-                              console.log('Evento guardado:', {
-                                classId: selectedEvent.id,
-                                id: selectedEvent.id, 
-                                instructorId: selectedEvent.instructorId,
-                                title: selectedEvent.title,
-                                description: selectedEvent.description,
-                                instructorName: selectedEvent.instructor,
-                                formattedDate: format(selectedEvent.date, "dd/MM/yyyy HH:mm"),
-                              });
-                              setShowModal(false);
-                              setSelectedEvent(null);
-                            }}
-                          >
-                            Unirme
+                        className="bg-[#C3C37E] hover:bg-[#5e46a5] text-white px-6 py-2 rounded-full font-semibold transition cursor-pointer"
+                        onClick={() => {
+                          agregarEvento({
+                            classId: selectedEvent.classId,
+                            instructorId: selectedEvent.instructorId,
+                            title: selectedEvent.title,
+                            description: selectedEvent.description,
+                            instructorName: selectedEvent.instructor,
+                            formattedDate: format(selectedEvent.date, "dd/MM/yyyy HH:mm"),
+                          });
+                          console.log('Evento guardado:', {
+                            classId: selectedEvent.classId,
+                            instructorId: selectedEvent.instructorId,
+                            title: selectedEvent.title,
+                            description: selectedEvent.description,
+                            instructorName: selectedEvent.instructor,
+                            formattedDate: format(selectedEvent.date, "dd/MM/yyyy HH:mm"),
+                          });
+
+                          setShowModal(false);
+                          setSelectedEvent(null);
+                        }}
+                      >
+                        Unirme
                       </button>
 
                     </>
