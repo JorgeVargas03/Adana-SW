1b152ef52b82dfe1eafb68cf12ddca433fc689b0
Jorge Vargas <joluvargaspa@ittepic.edu.mx>
2025-11-23 10:48:49 -0700

    invalidate

--- backend/utils/cloudinaryService.js
+++ backend/utils/cloudinaryService.js
@@ -45,7 +45,7 @@ exports.uploadImageToCloudinary = async (input, filename, mimeType, folder = und
 exports.destroyImageFromCloudinary = async (publicId) => {
   const folder = 'connixia-uploads/';
   try {
-    const result = await cloudinary.uploader.destroy(folder + publicId,{invalidate: true});
+    const result = await cloudinary.uploader.destroy(folder + publicId, { invalidate: true });
     return { result, success: true, message: "Imagen eliminada correctamente de Cloudinary" };
   } catch (error) {
     console.error('Error al destruir la imagen en Cloudinary:', error);
